a656e7cbf9aec33df8a1b38ece187bbd0cd4df6f
hector alcazar <hiad99@gmail.com>
2023-09-14 11:21:08 +0200

    feat: adding timestamp and visitor

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
-import React from 'react';
-
-import './styles.css';
+import React from "react";
+import TimeStamp from "../components/timestamp";
+import "./styles.css";
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          <TimeStamp></TimeStamp>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import Censored from "../components/censored";
+import HitCounter from "../components/visitor";
 
 function Home() {
   return (
     <main>
       <h1>Hello Next!</h1>
+      You are the visitor number{" "}
+      <Censored>
+        <HitCounter />
+      </Censored>
     </main>
   );
 }
